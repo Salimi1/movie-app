b5aaec14b5953c71a948ee3e42da899927484568
Farzad Salimi <shikhiarma@gmail.com>
2023-04-27 14:11:07 +0200

    I created the project folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import Navbar from './shared/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/shared/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import the Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import the Bootstrap JavaScript
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar;
